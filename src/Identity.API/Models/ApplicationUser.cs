b2fac4bab9f2f7768116f13de2283c585b5a396d
Muhammad Faiz Fikri <muhdfaizfikri91@gmail.com>
2021-02-08 23:09:23 +0800

    use different structure for identity.API

---
+++ src/Identity.API/Models/ApplicationUser.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace Identity.API.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string EmailAddress { get; set; }
+    }
+}
